e65cbdf8eaf7e6be268b0c8ff77fd3f5f26ff0a8
JaJa2006 <S10258662@connect.np.edu.sg>
2024-05-18 22:45:15 +0800

    Basic feature 2 update 2

---
+++ app/src/main/java/com/example/main_activity/Create_Card_Page.java
@@ -0,0 +1,56 @@
+package com.example.main_activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+
+public class Create_Card_Page extends AppCompatActivity {
+
+    ArrayList<Flashcard> flashcards = new ArrayList<Flashcard>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_create_card_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Intent receivingEnd = getIntent();
+        String Name = receivingEnd.getStringExtra("DeckName");
+
+        EditText EtCardName = findViewById(R.id.etCardName);
+        EditText EtCardFront = findViewById(R.id.etCardFront);
+        EditText EtCardBack = findViewById(R.id.etCardBack);
+
+        Button BtnCreateCard = findViewById(R.id.btnCreateCard);
+        BtnCreateCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Flashcard flashcard = new Flashcard(EtCardName.toString(),EtCardFront.toString(),EtCardBack.toString(),flashcards.size());
+                flashcards.add(flashcard);
+            }
+        });
+
+        Button BtnFinish = findViewById(R.id.btnFinish);
+        BtnFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/main_activity/Create_Deck_Page.java
+++ app/src/main/java/com/example/main_activity/Create_Deck_Page.java
@@ -1,6 +1,10 @@
 package com.example.main_activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,16 @@ public class Create_Deck_Page extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button BtnCreateDeck = findViewById(R.id.btnCreateDeck);
+        EditText EtDeckName = findViewById(R.id.etDeckName);
+        BtnCreateDeck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent CreateCardPage = new Intent(Create_Deck_Page.this,Create_Card_Page.class);
+                CreateCardPage.putExtra("DeckName",EtDeckName.toString());
+                startActivity(CreateCardPage);
+            }
         });
     }
 }
