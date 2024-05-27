3e2654deee4a7fc8f516028c94aae1b94024bf4c
JaJa2006 <S10258662@connect.np.edu.sg>
2024-05-27 16:07:26 +0800

    Set up firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.androidApplication)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -41,6 +42,7 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.activity:activity:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
