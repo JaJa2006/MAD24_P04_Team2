963212e8413634e39971872c23dd57d4c9662a4d
3i299492 <s10258529@connect.np.edu.sg>
2024-05-28 14:07:42 +0800

    update2

--- app/src/main/java/com/example/main_activity/Login_Page.java
+++ app/src/main/java/com/example/main_activity/Login_Page.java
@@ -33,6 +33,7 @@ public class Login_Page extends AppCompatActivity {
         final int maxLines = username.getMaxLines();
         final int pmaxLines = password.getMaxLines();
         TextWatcher textWatcher = new TextWatcher() {
+
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
             }
@@ -43,6 +44,9 @@ public class Login_Page extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
+                //checks whenever the text field is changed
+                //ensures that the text never goes beyond 1 line
+                //ensures no overflowing up or down
                 int lines = username.getLineCount();
                 if (lines > maxLines) {
                     // Remove the last character to prevent further input
@@ -57,33 +61,79 @@ public class Login_Page extends AppCompatActivity {
             }
         };
 
+        //implements the textwatcher
         username.addTextChangedListener(textWatcher);
         password.addTextChangedListener(textWatcher);
 
+        //assigns xml button to loginbutton
         Button loginbutton = findViewById(R.id.loginb);
         loginbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //checks the strings that are in the current text fields
                 String userstring = username.getText().toString();
                 String pwstring = password.getText().toString();
+
+                //checks if both username and password fields are empty
+                //if it is it will toss a toast that states it
                 if((userstring == null || userstring.isEmpty()) && (pwstring == null || pwstring.isEmpty())){
                     Toast.makeText(Login_Page.this, "Username and Password is empty", Toast.LENGTH_SHORT).show();
                 }
                 else {
+
+                    //checks if username is empty
+                    //if it is it will toss a toast that states it
                     if (userstring == null || userstring.isEmpty()) {
                         Toast.makeText(Login_Page.this, "Username is empty", Toast.LENGTH_SHORT).show();
                     }
-                    if (pwstring == null || pwstring.isEmpty()) {
+
+                    //checks if password is empty
+                    else if (pwstring == null || pwstring.isEmpty()) {
                         Toast.makeText(Login_Page.this, "Password is empty", Toast.LENGTH_SHORT).show();
                     } else {
-                        // Define an Intent to navigate to the NextActivity
-                        Intent intent = new Intent(Login_Page.this, OTP_Page.class);
+                        //code that takes the list of usernames currently in the database
 
-                        // Start the NextActivity
-                        startActivity(intent);
+                        if()
+                        {
+                            //check if username in editText field exists in database
+                            //move on to next check if it exists
+
+                            if(){
+                                //check if password is correct
+                                //successfully login if correct
+
+
+
+                                // Define an Intent to navigate to the NextActivity
+                                //only if the fields are all filled
+                                Intent intent = new Intent(Login_Page.this, OTP_Page.class);
+
+                                // Start the NextActivity
+                                startActivity(intent);
+
+                            }
+                            else{
+                                Toast.makeText(Login_Page.this, "Password is incorrect", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else{
+                            Toast.makeText(Login_Page.this, "Cannot find username", Toast.LENGTH_SHORT).show();
+                        }
 
                     }
                 }
+            }
+        });
+
+        Button regist = findViewById(R.id.regbtn);
+        regist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //set intent to go to register page
+                Intent intenn = new Intent(Login_Page.this,Register_Page.class);
+
+                startActivity(intenn);
             }
         });
     }
--- app/src/main/java/com/example/main_activity/Register_Page.java
+++ app/src/main/java/com/example/main_activity/Register_Page.java
@@ -1,6 +1,14 @@
 package com.example.main_activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +27,127 @@ public class Register_Page extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        EditText username = findViewById(R.id.userinputreg);
+        EditText password = findViewById(R.id.passinputreg);
+        EditText emaila = findViewById(R.id.emailinputreg);
+        final int maxLines = username.getMaxLines();
+        final int pmaxLines = password.getMaxLines();
+        final int emaxLines = emaila.getMaxLines();
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                int lines = username.getLineCount();
+                if (lines > maxLines) {
+                    // Remove the last character to prevent further input
+                    username.getText().delete(username.getSelectionStart() - 1, username.getSelectionStart());
+                }
+
+                int linesOther = password.getLineCount();
+                if (linesOther > pmaxLines) {
+                    // Remove the last character to prevent further input
+                    password.getText().delete(password.getSelectionStart() - 1, password.getSelectionStart());
+                }
+
+                int Thelinez = emaila.getLineCount();
+                if (Thelinez > emaxLines) {
+                    // Remove the last character to prevent further input
+                    emaila.getText().delete(emaila.getSelectionStart() - 1, emaila.getSelectionStart());
+                }
+            }
+        };
+
+        username.addTextChangedListener(textWatcher);
+        password.addTextChangedListener(textWatcher);
+        emaila.addTextChangedListener(textWatcher);
+
+        Button btnreg = findViewById(R.id.regbtn);
+        btnreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userstring = username.getText().toString();
+                String pwstring = password.getText().toString();
+                String emailstring = emaila.getText().toString();
+
+                //checking all test cases
+                if((userstring == null || userstring.isEmpty()) && (pwstring == null || pwstring.isEmpty()) && (emailstring == null || emailstring.isEmpty())){
+                    Toast.makeText(Register_Page.this, "All fields are empty", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    if ((userstring == null || userstring.isEmpty()) && (pwstring == null || pwstring.isEmpty())) {
+                        Toast.makeText(Register_Page.this, "Username and password is empty", Toast.LENGTH_SHORT).show();
+                    }
+                    else if ((pwstring == null || pwstring.isEmpty()) && (emailstring == null || emailstring.isEmpty())) {
+                        Toast.makeText(Register_Page.this, "Password and email is empty", Toast.LENGTH_SHORT).show();
+                    }
+                    else if ((userstring == null || userstring.isEmpty()) && (emailstring == null || emailstring.isEmpty())) {
+                        Toast.makeText(Register_Page.this, "Username and email is empty", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        if(userstring == null || userstring.isEmpty()){
+                            Toast.makeText(Register_Page.this, "Username is empty", Toast.LENGTH_SHORT).show();
+                        }
+                        else if(pwstring == null || pwstring.isEmpty()){
+                            Toast.makeText(Register_Page.this, "Password is empty", Toast.LENGTH_SHORT).show();
+                        }
+                        else if(emailstring == null || emailstring.isEmpty()){
+                            Toast.makeText(Register_Page.this, "Email is empty", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            //verifies if email address is in correct format
+                            //if it is not correct format it will enter if statement
+                            if(!Patterns.EMAIL_ADDRESS.matcher(emailstring).matches()){
+                                Toast.makeText(Register_Page.this, "Email is not valid", Toast.LENGTH_SHORT).show();
+                            }
+                            else if()
+                            {
+                                //code that takes the list of emails currently in the database
+
+                                if() {
+                                    //code that checks if email address does not exist in database
+                                    //email address to be used as identifier key
+
+
+                                }
+                                else{
+                                    Toast.makeText(Register_Page.this, "Email is taken", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                            else if()
+                            {
+                                // code that takes the list of usernames currently in the database
+
+                                if() {
+                                    //username will be unique
+                                    //check if username is taken
+
+
+                                }
+                                else{
+                                    Toast.makeText(Register_Page.this, "Username is taken", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                            else {
+                                // Define an Intent to navigate to the NextActivity
+                                Intent intent = new Intent(Register_Page.this, Login_Page.class);
+
+                                // Start the NextActivity
+                                startActivity(intent);
+                            }
+                        }
+
+                    }
+                }
+            }
         });
     }
 }
