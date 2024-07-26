a402292e9cea5462dbc7d2f2276c935940c4a955
JaJa2006 <S10258662@connect.np.edu.sg>
2024-07-26 22:14:34 +0800

    Update 7 to Jaden Stage 2 feature

--- app/src/main/java/com/example/main_activity/SongListAdapter.java
+++ app/src/main/java/com/example/main_activity/SongListAdapter.java
@@ -1,6 +1,7 @@
 package com.example.main_activity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
@@ -11,13 +12,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class SongListAdapter extends RecyclerView.Adapter<SongListViewHolder>{
+    boolean isVisible = false;
     ArrayList<String> data;
     MediaPlayer myMPlayer;
     int playlistID;
@@ -54,15 +59,17 @@ public class SongListAdapter extends RecyclerView.Adapter<SongListViewHolder>{
             String [] Songindicator = musicPlaylist.SongIndicator.split("`");
             SongIndicatorList = new ArrayList<String>(Arrays.asList(Songindicator));
         }
-        // set the music playlist data to the recycler view
+        // get the indicator value
+        String indicator = (SongIndicatorList.get(holder.getAdapterPosition()).matches("good")) ? "good" :
+                (SongIndicatorList.get(holder.getAdapterPosition()).matches("medium")) ? "medium" :"bad";
+        // set the view with the music playlist data
         holder.Song.setText(SongName);
+        holder.DeleteSong.setVisibility(isVisible ? View.VISIBLE : View.INVISIBLE);
         // set the song indicator
         holder.SongIndicator.setBackgroundResource(
-                (SongIndicatorList.get(holder.getAdapterPosition()).matches("good")) ? R.drawable.song_indicator_good :
-                        (SongIndicatorList.get(holder.getAdapterPosition()).matches("medium")) ? R.drawable.song_indicator_medium :
+                (indicator.matches("good")) ? R.drawable.song_indicator_good :
+                        (indicator.matches("medium")) ? R.drawable.song_indicator_medium :
                                 R.drawable.song_indicator_bad);
-        holder.SongIndicator.setText((SongIndicatorList.get(holder.getAdapterPosition()).matches("good")) ? "+" :
-                (SongIndicatorList.get(holder.getAdapterPosition()).matches("medium")) ? "=" : "-");
         // handle the testing of music when you tap the song name
         holder.Song.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -91,15 +98,52 @@ public class SongListAdapter extends RecyclerView.Adapter<SongListViewHolder>{
         holder.DeleteSong.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // create instant of dbhandler and delete playlist from there
-                MusicPlaylistDatabaseHandler dbHandler = new MusicPlaylistDatabaseHandler(v.getContext(), null, null, 1);
-                dbHandler.DeleteSongs(playlistID,holder.getAdapterPosition());
-                // refresh the page
-                Context context = holder.DeleteSong.getContext();
-                Intent refresh = new Intent(context,Manage_Playlist.class);
-                refresh.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                context.startActivity(refresh);
-                ((Manage_Playlist)context).finish();
+                // create the alert
+                AlertDialog alertDialog = new MaterialAlertDialogBuilder(v.getContext())
+                        .setTitle("Delete Song?")
+                        .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // create instant of dbhandler and delete Song from there
+                                MusicPlaylistDatabaseHandler dbHandler = new MusicPlaylistDatabaseHandler(v.getContext(), null, null, 1);
+                                dbHandler.DeleteSongs(playlistID,holder.getAdapterPosition());
+                                // refresh the page
+                                Context context = holder.DeleteSong.getContext();
+                                Intent refresh = new Intent(context,Manage_Playlist.class);
+                                refresh.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                                context.startActivity(refresh);
+                                ((Manage_Playlist)context).finish();
+                            }
+                        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // set the cancel button to close the dialog
+                                dialog.dismiss();
+                            }
+                        }).create();
+                // show the alert
+                alertDialog.show();
+            }
+        });
+        // set indicator text
+        holder.SongIndicator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = (indicator.matches("good")) ? "This song is a good song for studying." :
+                        (indicator.matches("medium")) ? "This song is average for studying." :
+                                "This song is bad for studying.";
+                // create the alert to show the info
+                AlertDialog alertDialog = new MaterialAlertDialogBuilder(v.getContext())
+                        .setTitle("Song Indicator")
+                        .setMessage(message)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        }).create();
+                // show the alert
+                alertDialog.show();
             }
         });
     }
@@ -113,5 +157,10 @@ public class SongListAdapter extends RecyclerView.Adapter<SongListViewHolder>{
             myMPlayer.release(); // clear the memory
             myMPlayer = null;
         }
+    }
+    // function to change the visibility of th delete button
+    public void DeleteSongVisibility() {
+        isVisible = !isVisible;
+        notifyDataSetChanged();
     }
 }
